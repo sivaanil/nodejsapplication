1f3ce49d15f4dc11e60217a42d61740629d1efae
siva anil <venkatalapati2009@gmail.com>
2016-04-27 18:32:24 +0530

    Edit functionality

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -4,14 +4,18 @@
 
 var raControllers = angular.module('raApp.controllers', []);
 
-raControllers.controller('userController', function($scope,userService,$routeParams) {
+raControllers.controller('userController', function($scope,userService,$routeParams,$location) {
 		userService.getData().success(function(data){
 				$scope.users = data;
 		});
 
 	$scope.edit = function(id){
-		console.log('+');
-		console.log(id);
+		$location.path('user/update/'+id);
+		userService.getUserDetails().success(function (response) {
+
+		})
+
+
 	};
 		
 	});
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var underscore = require('underscore');
+var url = require('url');
 var app = express();
 var cors = require('cors');
 app.use(cors({origin: '*'}));
@@ -16,12 +17,16 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.redirect('/index.html');
 });
+// To fetch all users
 app.get('/fetchusers',function(req, res){
 		db.getUsers(function (finalData) {
 			res.json(finalData);
 		});
-
-
+});
+// TO fetch single user details
+app.get('/fetchuser/:id',function(req, res){
+	var userId = req.params.id;
+	
 
 });
 app.listen(1321);
