4d7e37f77b5e79cb1d16327de7969b395d2f36b2
Siva Anil <venkatalapati2009@gmail.com>
2016-05-01 21:17:11 +0530

    Pagination edit and coming back to same page must be fixed

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -4,23 +4,36 @@
 
 var raControllers = angular.module('raApp.controllers', []);
 
-raControllers.controller('userController', function($scope,userService,$routeParams) {
+raControllers.controller('userController', function($scope,userService,$routeParams,$rootScope) {
 	$scope.customers = {};
+	$scope.userscount = 0;
+
 	// pagination variables
-	$scope.currentPage = 1
-		,$scope.itemsPerPage= 10
+
+	$scope.currentPage = 1,
+	$scope.itemsPerPage= 10
 		,$scope.pageSize = 5;
-	$scope.setPage = function (pageNo) {
-		$scope.currentPage = pageNo;
+
+	$scope.pagination = {
+		current : 5
+	};
+	this.q = $routeParams.q || '';
+
+	var params = {
+		offset : (($scope.currentPage - 1) * $scope.pageSize),
+		limit : $scope.pageSize,
+		q : this.q
 	};
 
-	$scope.pageChanged = function() {
-		console.log('Page changed to: ' + $scope.currentPage);
+	$scope.pageChanged = function(newPageNumber) {
+		console.log(newPageNumber);
+		$scope.currentPage = newPageNumber;
 	};
 
-		userService.getData().success(function(data){
-				$scope.customers = data.users;
-				$scope.userscount = data.userscount;
+
+		userService.getData(params).success(function(data){
+				$scope.users = data.users;
+				$scope.userscount = data.count;
 		});
     $scope.paraer = $routeParams || '';
 	$scope.profile = {};
@@ -33,8 +46,6 @@ raControllers.controller('userController', function($scope,userService,$routePar
 		})
 
 	}
-
-		
 	});
 
 
